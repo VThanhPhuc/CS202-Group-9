7e06b1ddac1528b224db62721fc6ffebba6b36ef
VThanhPhuc <99302070+VThanhPhuc@users.noreply.github.com>
2022-11-06 18:14:56 +0700

    update

--- CS202/CS202/Car.cpp
+++ CS202/CS202/Car.cpp
@@ -1,19 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <Windows.h>
-
-struct Coord {
-    int x, y;
-    Coord() {
-        x = 0;
-        y = 0;
-    }
-    Coord(int a, int b) {
-        x = a;
-        y = 4;
-    }
-};
-
+#include "Car.h"
+#include <vector>
 int getnext100(int x, int dir) {
     x = x + dir;
     if (x <= 0) {
@@ -24,18 +13,6 @@ int getnext100(int x, int dir) {
     }
     return x;
 }
-
-class Obstacle {
-protected:
-    Coord main;
-    Coord* hitbox;
-    int num;
-    int speed;
-    int direct;// 1: right, -1: left
-public:
-    Obstacle();
-    void move();
-};
 
 Obstacle::Obstacle() {
     main = Coord(0, 0);
@@ -50,19 +27,6 @@ void Obstacle::move() {
     main.x = getnext100(main.x, direct);
 }
 
-class Vehicle : public Obstacle {
-protected:
-    bool light;
-public:
-};
-
-class Car : public Vehicle {
-public:
-    Car();
-    Car(int x, int y, int sp, int dir);
-    void upmainC();
-    void moveC();
-};
 Car::Car() {
     num = 5;
     main = Coord(0, 0);
@@ -99,12 +63,56 @@ void Car::upmainC() {
 
 void Car::moveC() {
     move();
+    upmainC();
+}
+
+int Car::getmainx() {
+    return main.x;
+}
+CarLane::CarLane() {
+    num = 0;
+    speed = 0;
+    direct = 1;
+}
+
+CarLane::CarLane(int yLane, int number, int speed, int direct) {
+    num = number;
+    int dis = 100 / num;
+    for (int i = 0; i < num; i++) {
+        Car add(dis * i, yLane, speed, direct);
+        Lane.push_back(add);
+    }
+    this->speed = speed;
+    this->direct = direct;
+    pos = new int[number];
+    yLSFML = 50 * yLane;
+}
+
+void CarLane::moveL() {
+    for (int i = 0; i < num; i++) {
+        Lane[i].moveC();
+    }
+}
+
+void CarLane::sfml(sf::RenderWindow& window) {
+    sf::Texture pic;
+    pic.loadFromFile("car.png");
+    sf::Sprite* out = new sf::Sprite[num];
+    for (int i = 0; i < num; i++) {
+        pos[i] = Lane[i].getmainx() * 10;
+        out[i].setTexture(pic);
+        out[i].setPosition(pos[i], yLSFML);
+    }
+    for (int i = 0; i < num; i++) {
+        window.draw(out[i]);
+    }
 }
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
+    CarLane L(4, 5, 20, 1);
+    CarLane T(8, 3, 20, -1);
     while (window.isOpen())
     {
         sf::Event event;
@@ -115,8 +123,12 @@ int main()
         }
 
         window.clear();
-
+        L.sfml(window);
+        T.sfml(window);
+        
         window.display();
+        L.moveL();
+        T.moveL();
     }
 
     return 0;

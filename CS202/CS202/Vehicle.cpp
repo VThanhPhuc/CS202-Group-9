42727ae498d716a2e0bfc9095bc01439440e3b87
VThanhPhuc <99302070+VThanhPhuc@users.noreply.github.com>
2022-11-13 20:18:43 +0700

    update

--- CS202/CS202/Vehicle.cpp
+++ CS202/CS202/Vehicle.cpp
@@ -1,30 +1,20 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
 #include <Windows.h>
+#include "Vehicle.h"
 
 using namespace std;
 
-class CVEHICLE {
-private: 
-	int mX, mY;
-	int speed;
-public:
-	virtual void move();
-	virtual void draw(sf::RenderWindow window);
-	CVEHICLE();
-	CVEHICLE(int x, int y, int sp);
-	int getmX();
-	int getmY();
-	int getspeed();
-};
 
-void CVEHICLE::draw(sf::RenderWindow window){}
-int getnextmod200(int x, int sp) {
+
+void CVEHICLE::draw(sf::RenderWindow& window){}
+int getnextmod150(int x, int sp) {
 	x = x + sp;
 	if (x < 0)
-		return x + 200;
-	else if (x > 200)
-		return x + 200;
+		return x + 150;
+	else if (x > 150)
+		return x + 150;
 	else
 		return x;
 }
@@ -48,15 +38,10 @@ CVEHICLE::CVEHICLE(int x, int y, int sp) {
 	speed = sp;
 }
 void CVEHICLE::move() {
-	mX = getnextmod200(mX,speed);
+	mX = getnextmod150(mX,speed);
 }
 
-class CCar :public CVEHICLE {
-private:
-public:
-	void draw(sf::RenderWindow& window);
-	void move();
-};
+
  
 void CCar::move() {
 	CVEHICLE::move();
@@ -64,23 +49,18 @@ void CCar::move() {
 void CCar::draw(sf::RenderWindow& window) {
 	sf::Texture pic;
 	if (getspeed() > 0) {
-		pic.loadFromFile("car.png");
+		pic.loadFromFile("car1.png");
 	}
 	else {
-		pic.loadFromFile("car1.png");
+		pic.loadFromFile("car.png");
 	}
 	sf::Sprite out;
 	out.setTexture(pic);
-	out.setPosition(getmX()*30, getmY()*40);
+	out.setPosition(getmX()*10, getmY()*40);
 	window.draw(out);
 }
 
-class CTruck :public CVEHICLE {
-private:
-public:
-	void draw(sf::RenderWindow& window);
-	void move();
-};
+
 void CTruck::move() {
 	CVEHICLE::move();
 }
@@ -94,6 +74,127 @@ void CTruck::draw(sf::RenderWindow& window) {
 	}
 	sf::Sprite out;
 	out.setTexture(pic);
-	out.setPosition(getmX() * 30, getmY() * 40);
+	out.setPosition(getmX() * 10, getmY() * 40);
 	window.draw(out);
 }
+CANIMAL::CANIMAL() {
+	mX = 0;
+	mY = 0;
+	speed = 1;
+}
+CANIMAL::CANIMAL(int x, int y, int sp) {
+	mX = x;
+	mY = y;
+	speed = sp;
+}
+
+int CANIMAL::getmX() {
+	return mX;
+}
+int CANIMAL::getmY() {
+	return mY;
+}
+int CANIMAL::getspeed() {
+	return speed;
+}
+void CANIMAL::move() {
+	mX = getnextmod150(mX, speed);
+}
+void CANIMAL::draw(sf::RenderWindow& window) {}
+
+
+void CBIRD::draw(sf::RenderWindow& window) {
+	sf::Texture pic;
+	if (getspeed() > 0) {
+		pic.loadFromFile("bird.png");
+	}
+	else {
+		pic.loadFromFile("bird1.png");
+	}
+	sf::Sprite out;
+	out.setTexture(pic);
+	out.setPosition(getmX() * 10, getmY() * 40);
+	window.draw(out);
+}
+
+
+void CBIRD::move() {
+	CANIMAL::move();
+}
+void CDINO::draw(sf::RenderWindow& window) {
+	sf::Texture pic;
+	if (getspeed() > 0) {
+		pic.loadFromFile("dino.png");
+	}
+	else {
+		pic.loadFromFile("dino1.png");
+	}
+	sf::Sprite out;
+	out.setTexture(pic);
+	out.setPosition(getmX() * 10, getmY() * 40);
+	window.draw(out);
+}
+
+void CDINO::move() {
+	CANIMAL::move();
+}
+void CANIMAL::tell(sf::SoundBuffer& Buffer, sf::Sound& S){}
+void CDINO::tell(sf::SoundBuffer& Buffer, sf::Sound& S){
+
+}
+void CBIRD::tell(sf::SoundBuffer& Buffer, sf::Sound& S){
+	Buffer.loadFromFile("birdsound.wav");
+	S.setBuffer(Buffer);
+	
+}
+CCar::CCar(int x, int y, int sp) :CVEHICLE(x, y, sp) {}
+CTruck::CTruck(int x, int y, int sp) :CVEHICLE(x, y, sp) {}
+
+
+int main() {
+	CBIRD b;
+	sf::SoundBuffer Buffer;
+
+	sf::Sound s;
+	b.tell(Buffer, s);
+	while (true) {
+		s.play();
+		Sleep(2000);
+	}
+
+}
+
+
+
+
+//int main() {
+//	//sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
+//	sf::SoundBuffer buffer;
+//	buffer.loadFromFile("birdsound.wav");
+//
+//	sf::Sound sound;
+//	sound.setBuffer(buffer);
+//	sound.play();
+//	CTruck c(5,10,10);
+//	CBIRD b;
+//	sound.setLoop(TRUE);
+//	while (true) {
+//		sound.play();
+//	}
+//	/*while (window.isOpen())
+//		    {
+//		        sf::Event event;
+//		        while (window.pollEvent(event))
+//		        {
+//		            if (event.type == sf::Event::Closed)
+//		                window.close();
+//		        }
+//		
+//		        window.clear();
+//				c.draw(window);
+//		        window.display();
+//				b.tell();
+//				s.play();
+//		    }*/
+//
+//}
--- CS202/CS202/Vehicle.h
+++ CS202/CS202/Vehicle.h
@@ -1,1 +1,69 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+class CVEHICLE {
+private:
+	int mX, mY;
+	int speed;
+public:
+	virtual void move();
+	virtual void draw(sf::RenderWindow& window);
+	CVEHICLE();
+	CVEHICLE(int x, int y, int sp);
+	int getmX();
+	int getmY();
+	int getspeed();
+};
+
+class CCar :public CVEHICLE {
+private:
+public:
+	CCar(int x, int y, int sp);
+	void draw(sf::RenderWindow& window);
+	void move();
+};
+
+class CTruck :public CVEHICLE {
+private:
+public:
+	CTruck(int x, int y, int sp);
+	void draw(sf::RenderWindow& window);
+	void move();
+};
+
+class CANIMAL {
+private:
+	int mX, mY;
+	int speed;
+public:
+	virtual void move();
+	virtual void draw(sf::RenderWindow& window);
+	virtual void tell(sf::SoundBuffer& Buffer, sf::Sound& S);
+	
+	CANIMAL();
+	CANIMAL(int x, int y, int sp);
+	int getmX();
+	int getmY();
+	int getspeed();
+};
+
+class CBIRD :public CANIMAL {
+private:
+public:
+	void draw(sf::RenderWindow& window);
+	void move();
+	void tell(sf::SoundBuffer& Buffer, sf::Sound& S);
+};
+
+class CDINO :public CANIMAL {
+private:
+public:
+	void draw(sf::RenderWindow& window);
+	void move();
+	void tell(sf::SoundBuffer& Buffer, sf::Sound& S);
+	
+};

2eda7a378a51d0fa8107f8f0a5b9a5a26fa83095
HarryNgo21 <95170906+HarryNgo21@users.noreply.github.com>
2022-12-06 18:58:16 +0700

    update Menu.h

--- CS202/CS202/Menu.h
+++ CS202/CS202/Menu.h
@@ -11,46 +11,7 @@ private:
 	sf::Texture t;
 	sf::Texture button;
 public:
-	void InnitMenuBackground()
-	{
-		if (!this->t.loadFromFile("universe.png"))
-		{
-			cout << "Can not load" << endl;
-		};
-		if (!this->button.loadFromFile("button.png"/*,sf::IntRect(100,100,200,200))*/))
-		{
-			cout << "Can not load" << endl;
-		};
-		this->MenuBackground.setTexture(t);
-
-		this->button_start.setTexture(&this->button);
-		this->button_start.setSize(sf::Vector2f(200, 200));
-		this->button_start.setPosition(100, 600);
-
-		this->button_exit.setTexture(&this->button);
-		this->button_exit.setSize(sf::Vector2f(200, 200));
-		this->button_exit.setPosition(400, 600);
-	}
-	void draw(sf::RenderWindow& window)
-	{
-		InnitMenuBackground();
-		window.draw(this->MenuBackground);
-		window.draw(this->button_start);
-		window.draw(this->button_exit);
-	}
-	int IsMouseOver(sf::RenderWindow& window)
-	{
-		float mouseX = sf::Mouse::getPosition(window).x;
-		float mouseY = sf::Mouse::getPosition(window).y;
-
-		if (mouseX >= button_start.getPosition().x && mouseX <= (button_start.getPosition().x + 200) && mouseY <= (button_start.getPosition().y + 200) && mouseY >= button_start.getPosition().y)
-		{
-			return 1;
-		}
-		if (mouseX >= button_exit.getPosition().x && mouseX <= (button_exit.getPosition().x + 200) && mouseY <= (button_exit.getPosition().y + 200) && mouseY >= button_exit.getPosition().y)
-		{
-			return 2;
-		}
-		return 0;
-	}
+	void InnitMenuBackground();
+	void draw(sf::RenderWindow& window);
+	int IsMouseOver(sf::RenderWindow& window);
 };

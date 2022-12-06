8389ddce0b28c53ff9f001c1e908b33b45b91bc1
HarryNgo21 <95170906+HarryNgo21@users.noreply.github.com>
2022-12-06 18:59:15 +0700

    update main.cpp

--- CS202/CS202/main.cpp
+++ CS202/CS202/main.cpp
@@ -9,6 +9,7 @@ int main()
 	CCar b(100, 10, 10);
 	CCar c(900, 10, -5);
 	CLight l;
+	Menu c1;
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -16,8 +17,22 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+
+				if (c1.IsMouseOver(window) == 1)
+				{
+					cout << "Start" << endl;
+				}
+				else if (c1.IsMouseOver(window) == 2)
+				{
+					cout << "Exit" << endl;
+				}
+				else cout << "No" << endl;
+			}
 		}
 		window.clear();
+		c1.draw(window);
 		b.draw(window);
 		c.draw(window);
 		window.display();

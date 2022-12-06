57311428c6750da0e0982a1bd1c6af0cc7e9a6cf
HarryNgo21 <95170906+HarryNgo21@users.noreply.github.com>
2022-12-06 18:55:11 +0700

    Innit Menu function

--- CS202/CS202/Menu.h
+++ CS202/CS202/Menu.h
@@ -1,1 +1,34 @@
 #pragma once
+#include "Vehicle.h"
+
+class Menu
+{
+private:
+	sf::Sprite MenuBackground;
+	sf::RectangleShape button_start;
+	sf::RectangleShape button_exit;
+
+	sf::Texture t;
+	sf::Texture button;
+public:
+	void InnitMenuBackground()
+	{
+		if (!this->t.loadFromFile("universe.png"))
+		{
+			cout << "Can not load" << endl;
+		};
+		if (!this->button.loadFromFile("button.png"/*,sf::IntRect(100,100,200,200))*/))
+		{
+			cout << "Can not load" << endl;
+		};
+		this->MenuBackground.setTexture(t);
+
+		this->button_start.setTexture(&this->button);
+		this->button_start.setSize(sf::Vector2f(200, 200));
+		this->button_start.setPosition(100, 600);
+
+		this->button_exit.setTexture(&this->button);
+		this->button_exit.setSize(sf::Vector2f(200, 200));
+		this->button_exit.setPosition(400, 600);
+	}
+};

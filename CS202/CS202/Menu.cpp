e1fef51517b5e41304fc213343d338d0daec77ea
HarryNgo21 <95170906+HarryNgo21@users.noreply.github.com>
2022-12-07 11:20:29 +0700

    update menu.cpp

--- CS202/CS202/Menu.cpp
+++ CS202/CS202/Menu.cpp
@@ -1,32 +1,50 @@
 #include "Menu.h"
-void Menu::InnitMenuBackground()
+void InnitMenuBackground()
 {
 	if (!this->t.loadFromFile("universe.png"))
 	{
 		cout << "Can not load" << endl;
 	};
-	if (!this->button.loadFromFile("button.png"/*,sf::IntRect(100,100,200,200))*/))
+	if (!this->s_button.loadFromFile("s_button.png"))
+	{
+		cout << "Can not load" << endl;
+	};
+	if (!this->exit_button.loadFromFile("exit_button.png"/*,sf::IntRect(100,100,200,200))*/))
 	{
 		cout << "Can not load" << endl;
 	};
 	this->MenuBackground.setTexture(t);
 
-	this->button_start.setTexture(&this->button);
+	this->button_start.setTexture(&this->s_button);
 	this->button_start.setSize(sf::Vector2f(200, 200));
 	this->button_start.setPosition(100, 600);
 
-	this->button_exit.setTexture(&this->button);
+	this->button_exit.setTexture(&this->exit_button);
 	this->button_exit.setSize(sf::Vector2f(200, 200));
 	this->button_exit.setPosition(400, 600);
 }
-void Menu::draw(sf::RenderWindow& window)
+void draw_menu(sf::RenderWindow& window)
 {
 	InnitMenuBackground();
 	window.draw(this->MenuBackground);
 	window.draw(this->button_start);
 	window.draw(this->button_exit);
 }
-int Menu::IsMouseOver(sf::RenderWindow& window)
+void draw_gameplay(sf::RenderWindow& window)
+{
+	if (!this->b_button.loadFromFile("b_button.png"))
+	{
+		cout << "Can not load" << endl;
+	};
+	this->button_back.setTexture(&this->b_button);
+	this->button_back.setSize(sf::Vector2f(200, 200));
+	this->button_back.setPosition(1600, 100);
+
+	this->button_start.setPosition(-1000, -1000);
+	this->button_exit.setPosition(-1000, -1000);
+	window.draw(this->button_back);
+}
+int IsMouseOver(sf::RenderWindow& window)
 {
 	float mouseX = sf::Mouse::getPosition(window).x;
 	float mouseY = sf::Mouse::getPosition(window).y;
@@ -38,6 +56,17 @@ int Menu::IsMouseOver(sf::RenderWindow& window)
 	if (mouseX >= button_exit.getPosition().x && mouseX <= (button_exit.getPosition().x + 200) && mouseY <= (button_exit.getPosition().y + 200) && mouseY >= button_exit.getPosition().y)
 	{
 		return 2;
+	}
+	return 0;
+}
+int CheckMouseGamePlay(sf::RenderWindow& window)
+{
+	float mouseX = sf::Mouse::getPosition(window).x;
+	float mouseY = sf::Mouse::getPosition(window).y;
+
+	if (mouseX >= button_back.getPosition().x && mouseX <= (button_back.getPosition().x + 200) && mouseY <= (button_back.getPosition().y + 200) && mouseY >= button_back.getPosition().y)
+	{
+		return 1;
 	}
 	return 0;
 }

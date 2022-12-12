3aa0a341866b3b1b8c785735a7c91c34a3a09024
tringuyen123 <95064327+tringuyen2711@users.noreply.github.com>
2022-12-12 23:01:07 +0700

    create a class CARLANE

---
+++ CS202/CS202/CARLANE.cpp
@@ -0,0 +1,87 @@
+#include "CARLANE.h"
+
+
+CARLANE::CARLANE(sf::Vector2f pos) : CROAD()
+{
+	// create texture
+
+	texture = &LoadPic::GetIns().texture[file];
+	out.setTexture(*texture);
+	// main
+	mX = pos.x;
+	mY = pos.y /*- Constants::HeightRoad + Constants::HeightRoad*/;
+	if (rand() % 6 == 0)
+	{
+		light = CLight(mX, mY);
+	}
+}
+
+CARLANE::CARLANE(float index) : CROAD()
+{
+
+	// main
+	mX = 0;
+	mY = index * Constants::HeightRoad - Constants::Height_HiddenRoad;
+
+	// crate texture
+
+	texture = &LoadPic::GetIns().texture[file];
+	out.setTexture(*texture);
+	out.setPosition(mX, mY);
+
+
+
+	if (rand() % 6 == 0) light = CLight(mX, mY);
+	//carli.initGame(mX, mY);
+}
+
+CARLANE::CARLANE(float x, float y, bool traffic) :CROAD(x, y)
+{
+	// create texture
+	texture = &LoadPic::GetIns().texture[file];
+	out.setTexture(*texture);
+	out.setPosition(mX, mY);
+
+	//main
+	if (traffic)
+	{
+		light = CLight(mX, mY);
+	}
+}
+
+CARLANE::~CARLANE()
+{
+	carli.~CCARLIST();
+}
+
+void CARLANE::drawSubObj(sf::RenderWindow& window)
+{
+	if (light.Traffic())
+	{
+		light.draw(window);
+	}
+	carli.draw(window);
+}
+
+void CARLANE::shiftObj(char shift)
+{
+	carli.shiftObject(shift);
+	if (light.Traffic())
+	{
+		light.shiftObject(shift);
+	}
+}
+
+void CARLANE::update(sf::RenderWindow& window)
+{
+	if (light.Traffic())
+	{
+		light.update(window);
+	}
+	carli.update(mX, mY, window, light);
+}
+
+deque<COBJECT*>* CARLANE::getObjLi()
+{
+	return carli.getCarList();
+}
---
+++ CS202/CS202/CARLANE.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "CROAD.h"
+#include <deque>
+#include "COBJECT.h"
+
+class CARLANE : public CROAD
+{
+private:
+	CLight light;
+	const string file = "Pavement_bottom";
+	CCARLIST carli;
+
+public:
+	// Constructor | destructor
+	CARLANE(sf::Vector2f pos);
+	CARLANE(float index = 1);
+	CARLANE(float x, float y, bool traffic);
+	~CARLANE();
+
+	// other function
+	void drawSubObj(sf::RenderWindow& window);
+	void shiftObj(char shift);
+	void update(sf::RenderWindow& window);
+
+	deque<COBJECT*>* getObjLi();
+
+	//sound
+
+	//save to file
+
+};
+

39229678e78f219cb1827f08307608b95e97cfe0
tringuyen123 <95064327+tringuyen2711@users.noreply.github.com>
2022-12-12 22:33:38 +0700

    create constants

---
+++ CS202/CS202/Constants.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+namespace Constants
+{
+	//window
+	const float Height_screen = 700;
+	const float width_screen = 1500;
+
+	// velocity
+	const float ShiftVelocity = 70;
+	const float VelcocityX = 0.014f;
+
+	const int widthVehicle = 80;
+	const int heightVehicle = 80;
+
+	// Lane
+	const int MAX_ROAD = 12;
+	const float WidthRoad = 1500;
+	const float HeightRoad = 100;
+
+	//Car
+	const int NCar = 15; // number of car
+	const string Cartype[] = { "car1","car","truck","truck1" };
+
+	// type
+	const int car = 1;
+	const int animal = 2;
+
+
+	const float Width_hiddenRoad = 1500;
+	const float Height_HiddenRoad = 100;
+}

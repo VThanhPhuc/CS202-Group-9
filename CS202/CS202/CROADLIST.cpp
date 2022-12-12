62ecd6ca7fdb9f0f53e7f03265b0f0b45e9a8084
tringuyen123 <95064327+tringuyen2711@users.noreply.github.com>
2022-12-12 23:03:10 +0700

    Create class CROADLIST

---
+++ CS202/CS202/CROADLIST.cpp
@@ -0,0 +1,99 @@
+#include "CROADLIST.h"
+
+CROADLIST::CROADLIST(CPEOPLE* player)
+{
+	this->player = player;
+	mX = mY = 0;
+	//mY_Origin = Constants::Height_HiddenRoad / 1.2;
+}
+
+CROADLIST::~CROADLIST()
+{
+	for (auto it : roadList)
+	{
+		delete it;
+	}
+}
+
+void CROADLIST::initRoad()
+{
+	while (CROAD::getNumRoads() <= Constants::MAX_ROAD)
+	{
+		roadList.push_front(createRoad(Constants::MAX_ROAD - CROAD::getNumRoads()));
+	}
+}
+
+
+CROAD* CROADLIST::createRoad(sf::Vector2f pos)
+{
+	//RoadType type = RoadType(rand() % LAST);
+	//if (type == LANE)
+	//{
+
+	return new CARLANE(pos);
+	//}
+
+}
+
+CROAD* CROADLIST::createRoad(float index)
+{
+
+	//RoadType type = RoadType(rand() % LAST);
+	int diff = 1;
+
+	//if (type == LANE)
+	//{
+	return new CARLANE(index - diff);
+	//}
+
+}
+
+
+void CROADLIST::shiftObj(char shift)
+{
+	if (shift == 'U' || shift == 'u')
+	{
+		mY += Constants::ShiftVelocity;
+		for (auto it : roadList) it->shiftObject(shift);
+
+	}
+	else if ((shift == 'D' || shift == 'd'))
+	{
+		mY -= Constants::ShiftVelocity;
+		for (auto it : roadList) it->shiftObject(shift);
+	}
+
+}
+
+void CROADLIST::draw(sf::RenderWindow& window)
+{
+	for (auto it : roadList)
+	{
+		it->draw(window);
+		it->drawSubObj(window);
+	}
+}
+
+void CROADLIST::update(sf::RenderWindow& window)
+{
+	if (mX > 1500)
+	{
+		roadList.push_front(createRoad(roadList.front()->getPos() + sf::Vector2f(0, 100)));
+		roadList.front()->draw(window);
+		mX = 0;
+	}
+	if (roadList.back()->checkoutwindow(window))
+	{
+		roadList.push_front(createRoad(roadList.front()->getPos()));
+		roadList.front()->draw(window);
+
+		CROAD* tmp = roadList.back();
+		roadList.pop_back();
+		delete tmp;
+	}
+	for (auto it : roadList)
+	{
+		it->update(window);
+
+	}
+}
---
+++ CS202/CS202/CROADLIST.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <deque>
+#include "CARLANE.h"
+#include "People.h"
+
+enum RoadType { LANE, LAST };
+
+class CROADLIST
+{
+private:
+	friend class CGAME;
+	deque<CROAD*> roadList;
+
+	CPEOPLE* player;
+
+	float mX;
+	float mY;
+
+
+public:
+	CROADLIST(CPEOPLE* player);
+	~CROADLIST();
+	void initRoad();
+
+	void shiftObj(char shift);
+	void draw(sf::RenderWindow& window);
+	void update(sf::RenderWindow& window);
+	CROAD* createRoad(sf::Vector2f pos);
+	CROAD* createRoad(float index = 1);
+
+	//sound
+
+
+	// save to file | load from file
+
+};
+

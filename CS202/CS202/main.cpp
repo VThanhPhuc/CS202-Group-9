cf218d1b5e39f93dde93d4eebf1fb0290e062c3f
HarryNgo21 <95170906+HarryNgo21@users.noreply.github.com>
2022-12-23 22:41:07 +0700

    update main.cpp

--- CS202/CS202/main.cpp
+++ CS202/CS202/main.cpp
@@ -2,62 +2,11 @@
 #include "CARLANE.h"
 #include "Light.h"
 #include "CGAME.h"
-
-//
-//int main()
-//{
-//	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
-//	CCar b(100, 10, 10);
-//	CCar c(900, 10, -5);
-//	CLight l;
-//	Menu c1;
-//	while (window.isOpen())
-//	{
-//		sf::Event event;
-//		while (window.pollEvent(event))
-//		{
-//			if (event.type == sf::Event::Closed)
-//				window.close();
-//			if (event.type == sf::Event::MouseButtonPressed)
-//			{
-//
-//				if (c1.IsMouseOver(window) == 1)
-//				{
-//					cout << "Start" << endl;
-//				}
-//				else if (c1.IsMouseOver(window) == 2)
-//				{
-//					cout << "Exit" << endl;
-//				}
-//				else cout << "No" << endl;
-//			}
-//		}
-//		window.clear();
-//		c1.draw(window);
-//		b.draw(window);
-//		c.draw(window);
-//		window.display();
-//		b.move(l);
-//		c.move(l);
-//		l.update();
-//		Sleep(100);
-//		if (crash(b, c) == true) {
-//			window.close();
-//		}
-//	}
-//
-//	return 0;
-//}
-
+#include "Background.h"
 int main()
 {
-
-	CGAME game;
-	game.run();
-	system("pause");
-	cout << "OK to out!";
-	
-
+	Menu c1;
+	c1.Begin();
 
 	return 0;
 
